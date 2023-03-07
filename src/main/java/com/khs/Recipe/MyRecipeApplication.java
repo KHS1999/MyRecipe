56f49c16d1e105b052eff41efe9258bf414bfc9b
DESKTOP-F2HFMDB\user <ip6008@naver.com>
2023-03-07 20:59:00 +0900

    세팅

---
+++ src/main/java/com/khs/Recipe/MyRecipeApplication.java
@@ -0,0 +1,16 @@
+package com.khs.Recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class MyRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRecipeApplication.class, args);
+		
+	}
+	
+}
